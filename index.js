3a5b8f5fe8466246212877383c1255f061f22680
Kier Anthony Dalit <kier0729@gmail.com>
2024-02-19 20:57:34 +0800

    Add initial filter option for months

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = 4000;
 const saltRounds = 10;
 const CLIENT_URI = "http://localhost:3000";
-// let temporaryUser;
+let id;
 
 //Needed to send data // use to link server(node/express) the frontend(react)
 ////////////////////////////////////////////////////////////
@@ -32,11 +32,8 @@ app.use(session({
     secret: "JDG",
     resave: false,
     saveUninitialized: true,
-        // domain: 'localhost:3000',
     cookie: { //HOW LONG COOKIE WILL BE SAVE (1000 miliseconds = 1 second * 60 = 1 min * 60 = 1hr )
       maxAge: 1000 * 60 * 60,
-      // httpOnly: true,
-      // secure: true,
     }
   })
 );
@@ -65,24 +62,40 @@ let data =[
 
 //for acquiring the data in the database
 ////////////////////////////////////////////////////////////
-async function fetchData(){
-    try{
-        //TO_CHAR(entry_date, 'MM/DD/YYYY') AS date to get the date and set date format in postgresql
-        const result = await db.query("SELECT id, TO_CHAR(entry_date, 'MM/DD/YYYY') AS date, entry_merchant AS merchant, entry_amount AS amount FROM user_entry ORDER BY id ASC");
-        let myData=[];
-        result.rows.forEach((items) => { //transferring each row data to myData array
-            myData.push(items);
-        });
-        return myData; //return/pass myData value if fetchData is called
-      } catch (error) {
-        console.log(error.message);
-      }
+async function fetchData(month){
+  try{
+      //TO_CHAR(entry_date, 'MM/DD/YYYY') AS date to get the date and set date format in postgresql
+      const result = await db.query("SELECT id, TO_CHAR(entry_date, 'MM/DD/YYYY') AS date, entry_merchant AS merchant, entry_amount AS amount FROM user_entry WHERE entry_id = $1 AND EXTRACT(MONTH FROM entry_date) = $2 ORDER BY id ASC",[id, month]);
+      let myData=[];
+      result.rows.forEach((items) => { //transferring each row data to myData array
+           myData.push(items);
+      });
+      return myData; //return/pass myData value if fetchData is called
+    } catch (error) {
+      console.log(error.message);
+    }
 }
+
+//for acquiring the data in the database for all months
+////////////////////////////////////////////////////////////
+// async function fetchData(){
+//   try{
+//       //TO_CHAR(entry_date, 'MM/DD/YYYY') AS date to get the date and set date format in postgresql
+//       const result = await db.query("SELECT id, TO_CHAR(entry_date, 'MM/DD/YYYY') AS date, entry_merchant AS merchant, entry_amount AS amount FROM user_entry WHERE entry_id = $1 ORDER BY id ASC",[id]);
+//       let myData=[];
+//       result.rows.forEach((items) => { //transferring each row data to myData array
+//            myData.push(items);
+//       });
+//       return myData; //return/pass myData value if fetchData is called
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+// }
 
 async function addData(rcvd){
     try{
-        const result = await db.query("INSERT INTO user_entry (entry_date, entry_merchant, entry_amount) VALUES ($1,$2,$3)",
-        [rcvd.date, rcvd.merchant, rcvd.amount]);
+        const result = await db.query("INSERT INTO user_entry (entry_date, entry_merchant, entry_amount, entry_id) VALUES ($1,$2,$3,$4)",
+        [rcvd.date, rcvd.merchant, rcvd.amount, rcvd.entry_id]);
       } catch (error) {
         console.log(error.message);
       }
@@ -129,15 +142,15 @@ passport.use( "local",
             } else { //FAIL authentication
               //cb returns (noerror(null), false(user is not authenticated))
               // console.log("Authentication Failed!");
-              return cb(null, false);
+              return cb(err, false);
             }
           }
         });
       } else {
-        return cb("User not found");
+        return cb(null, false);
       }
     } catch (err) {
-      return cb(err);
+      return cb(null, false);
     }
   })
 );
@@ -152,28 +165,43 @@ passport.deserializeUser((user, cb) => {
   console.log("deserializeUser");
 });
 
-app.get("/", async (req,res)=>{
-    data = await fetchData();//setting the value of data using fetchData (check fetchData)
-    res.send(data);
-
-    // res.redirect(`${CLIENT_URI}/Home`);
+app.post("/fetch", async (req,res)=>{
+    console.log("/fetch");
+    const month = req.body.selectedMonth;
+    console.log(month);
+    console.log(id);
+    if(req.user){
+        data = await fetchData(month);//setting the value of data using fetchData (check fetchData)
+        console.log("fetch");
+        res.send(data);
+    }  
 });
 
 app.get("/IsLogin", (req,res,)=>{
   console.log("IsLogin");
-  console.log(req.isAuthenticated());
-  console.log(req.user);
-  req.isAuthenticated() ?
-  res.send(req.user) 
-  : res.send(null);
+  if(req.isAuthenticated()){
+    id = req.user.id;
+    res.send(req.user);
+  } else {
+    res.send({...req.user, password:"Mismatch"});
+  }
 });
 
-app.get("/Failed", (req,res)=>{
-  const auth = req.isAuthenticated();
-  console.log(auth);
+app.get("/IsFailed", (req,res)=>{
   console.log("IsFailed");
-  // console.log(temporaryUser);
-  res.send(null);
+  console.log(req.isAuthenticated());
+  // console.log(req.user);
+  res.send({...req.user, notFound:"No Match"});
+});
+
+app.get("/Logout", (req, res)=>{
+  setTimeout(()=>{
+    req.logout(()=>{
+    console.log("User Logout!");
+    req.session.destroy();
+    res.clearCookie("cookiename").send(null);//sending back something(null) so .then will execute in frontend
+  });
+}, 300);
 });
 
 //ROUTE FOR ADDING DATA to the DATABASE
@@ -185,32 +213,33 @@ app.get("/Failed", (req,res)=>{
 
     const received = req.body;
     addData(received);
-    data = await fetchData();//setting the value of data using fetchData (check fetchData)
-    res.send(data);
-});
+      data = await fetchData(received.selectedMonth);//setting the value of data using fetchData (check fetchData)   
+      res.send(data);
+    });
 
-app.post("/Login", (req, res) => {passport.authenticate("local", {
-  successRedirect: "/IsLogin",
-  failureRedirect: "/IsFailed"
-})(req, res);
-}
-);
+app.post("/Login", async (req, res) => {
+  setTimeout(async() => {
+    const result = await db.query("SELECT * FROM user_cred WHERE user_email = $1", [
+      req.body.username,
+    ]);
+    if (result.rows.length > 0) {
+      passport.authenticate("local", {
+      successRedirect: "/IsLogin",
+      failureRedirect: "/IsLogin"
+     })(req, res);
+    } else {
+      res.redirect("/IsFailed");
+    }
+  }, 300);
+  
+});
 
 // app.post("/Login", 
 // (req, res)=>{ passport.authenticate("local", function(err, user){
 //   if(user){
-//       // req.login(user, (err)=>{//needed for calling serialize and deserialize
-//       //   if (err){} else {  
-//       //   console.log(`Login under: ${user.user_email}`);//here data is from serializing
-//       //     temporaryUser=user;
-//       //     // res.send(user);
-//       //     res.redirect("/IsLogin");
-//       //   }
-//       // });
-//       req.login(user, ()=>{
+//       req.login(user, ()=>{ //needed for calling serialize and deserialize
 //         res.redirect("/IsLogin");
 //       });
-
 //       } else {
 //           console.log("Access denied!");
 //           res.send(null);
@@ -220,8 +249,8 @@ app.post("/Login", (req, res) => {passport.authenticate("local", {
 // );
 
 app.post("/Register", async (req,res)=>{
-  const {username, password, fname, lname} = req.body;
-
+  setTimeout(async() => {
+    const {username, password, fname, lname} = req.body;
   try {
     const checkResult = await db.query("SELECT * FROM user_cred WHERE user_email = $1", [
       username,
@@ -250,8 +279,7 @@ app.post("/Register", async (req,res)=>{
             }
             else{
               console.log("Authentication success!");
-              temporaryUser=user;
-              res.send(user);
+              res.redirect("/IsLogin");
             }  
           })
         }
@@ -260,8 +288,8 @@ app.post("/Register", async (req,res)=>{
   } catch (err) {
     console.log(err);
   }
-
 console.log("Register initiated");
+  }, 300);
 });
 
 app.patch("/update", async (req,res)=>{
@@ -271,7 +299,7 @@ app.patch("/update", async (req,res)=>{
 
     const received = req.body;
     updateData(received);
-    data = await fetchData();//setting the value of data using fetchData (check fetchData)
+    data = await fetchData(received.selectedMonth);//setting the value of data using fetchData (check fetchData)
     res.send(data);
 });
 
@@ -281,8 +309,9 @@ app.delete("/delete", async (req,res)=>{
     // data = data.filter(function(item, index){return(index != id)});
     // res.send(data);
     const received = req.body;
+    console.log(received);
     deleteData(received);
-    data = await fetchData();//setting the value of data using fetchData (check fetchData)
+    data = await fetchData(received.selectedMonth);//setting the value of data using fetchData (check fetchData)
     res.send(data);
 });
 
