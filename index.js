553b364e502fe34174a33a585215d54a87e36913
Kier Anthony Dalit <kier0729@gmail.com>
2024-03-11 18:39:50 +0800

    fixing samesite issue

--- index.js
+++ index.js
@@ -29,16 +29,16 @@ const corsOptions = {
   credentials: true,
 }
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://project-tracker-8zss.onrender.com"); // update to match the domain you will make the request from
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://project-tracker-8zss.onrender.com"); // update to match the domain you will make the request from
+//   // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.use(cors(corsOptions));
 
